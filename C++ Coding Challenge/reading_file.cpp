b314636c1fc46326c49b821963555b04f064126d
emmanuel512 <emmanud@udel.edu>
2017-02-15 12:31:49 -0500

    Update reading_file.cpp

--- C++ Coding Challenge/reading_file.cpp
+++ C++ Coding Challenge/reading_file.cpp
@@ -1,22 +1,25 @@
 /****************************************
-*Emmanuel Banjo
-*Honeywell C++ Coding Challenge
+Emmanuel Banjo
+Honeywell C++ Coding Challenge
 *****************************************/
 
 #include <iostream>
 #include <fstream>
-#include <string>
 #include <vector> 
 #include <cstdlib>
 #include <cstdio>
 #include <algorithm>
+
 using namespace std;
+
+/**********Variable Declarartion************/
+std::ifstream infile; //file object
 
 /**********Function Declaration*************/
 void printStringBuffer(vector<string> &buffer, int size);
 vector<string> fileStringBuffer(std::ifstream &file, char *fileName);
 int posOfString(int pos, vector<string> &a, vector<string> &b);
-
+void printOccurences(vector<string> &buffer);
 
 
 /**********Function Implementation**********/
@@ -56,6 +59,39 @@ int posOfString(int pos, vector<string> &a, vector<string> &b)
 	}
 }
 
+void printOccurences(vector<string> &buffer)
+{
+	vector<string> comp; //Buffer holding each unique occurence of line 
+	vector<int> freq; //Buffer holding integer values of line occurences
+
+	bool isPresent;
+
+	for(int i = 0; i < buffer.size()-1; i++)
+	{
+		isPresent = (std::find(comp.begin(), comp.end(), buffer[i]) != comp.end());
+
+		if(!isPresent)
+		{	
+			//std::cout << "String not found, appending string to compare vector" << std::endl;
+			comp.push_back(buffer[i]);
+			freq.push_back(1);
+		}
+		else
+		{
+			//std::cout << "Same string found" << std::endl;
+			int position = posOfString(i, buffer, comp);
+			freq[position]++;
+		}
+	}
+
+
+	for(int i = 0; i < freq.size(); i++)
+	{
+		std::cout << freq[i] << " " << comp[i] << std::endl;
+	}
+}
+
+
 int main(int argc, char *argv[])
 {
 	//Check if there is a valid file to be read
@@ -64,41 +100,22 @@ int main(int argc, char *argv[])
 		std::cout << "Please enter a file to read" << std::endl;
 		exit(1);
 	}
-
-	std::ifstream infile;
 	
 	std::cout << "Reading from the file" << std::endl;
 	
 	vector<string> v = fileStringBuffer(infile, argv[1]); //Buffer holding lines from file
-	vector<string> comp; //Buffer holding each unique occurence of line 
-	vector<int> freq; //Buffer holding integer values of line occurences
 
-	bool isPresent;
+	std::cout << "File has " << v.size()-1 << " lines" << std::endl;
 
-	for(int i = 0; i < v.size()-1; i++)
-	{
-		isPresent = (std::find(comp.begin(), comp.end(), v[i]) != comp.end());
+	std::cout << "Below are the lines:" << std::endl; 
 
-		if(!isPresent)
-		{	
-			//std::cout << "String not found, appending string to compare vector" << std::endl;
-			comp.push_back(v[i]);
-			freq.push_back(1);
-		}
-		else
-		{
-			//std::cout << "Same string found" << std::endl;
-			int position = posOfString(i, v, comp);
-			freq[position]++;
-		}
-	}
+	printStringBuffer(v, v.size());
+
+	//readStringBuffer(comp, comp.size()); //Debugging..show the vector of unique strings
+
+	std::cout << "Below are the unique lines and the number of times they occur:" << std::endl;
+
+	printOccurences(v);
 	
-	//printStringBuffer(comp, comp.size()); //Debugging..show the vector of unique strings
-
-	for(int i = 0; i < freq.size(); i++)
-	{
-		std::cout << freq[i] << " " << comp[i] << std::endl;
-	}
-
 	return 0;
 }
